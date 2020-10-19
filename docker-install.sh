3ee91789b98fe5724eb14478511a183e4ba808ed
doctorai-in <54732569+doctorai-in@users.noreply.github.com>
2020-10-19 11:09:33 +0530

    Add files via upload

---
+++ docker-install.sh
@@ -0,0 +1,35 @@
+# Install Docker
+echo **********INSTALLING DOCKER IMAGE************* 
+
+sudo apt-get -y update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get -y update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+echo **********INSTALLATION FINISHED*************
+
+echo **********setup username***************
+read -p 'NodeName: ' user
+sudo adduser $user --disabled-password
+sudo passwd $user
+sudo usermod -aG sudo $user
+echo ************SETTING UP DOCKER AS NON-ROOT*************
+sudo usermod -aG docker $user
+sudo su - $user
